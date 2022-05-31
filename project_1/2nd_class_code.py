91fd802639cc8bd17b3ba6c03037ea445f2e450c
Sam Williams <55559584+samuel-m-williams@users.noreply.github.com>
2022-05-30 21:42:12 -0600

    Project 1 Upload

---
+++ project_1/2nd_class_code.py
@@ -0,0 +1,15 @@
+#%%
+import pandas as pd
+import altair as alt
+import altair_saver
+
+# "They have a name for that.  Its called seasonal depression."  Sister Allen :)
+# I am grateful to look sharp in my work clothes.  I like looking sharp.
+#  I am grateful for our teacher.  She seems like a fun lady
+# D&A dept should look at persistence on a class and semester basis
+# based on what day of the week the semester starts
+
+# df[col_name] is the same as df.col_name
+
+
+
